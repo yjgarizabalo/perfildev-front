34f57ba3a56f7410922c14a831845ae003bf07bc
yairg <yjgarizabalo@gmail.com>
2019-11-26 23:11:31 -0500

    create  mkdir  server config

---
+++ src/index.js
@@ -0,0 +1,45 @@
+const express = require('express')
+const morgan = require('morgan')
+const exphbs = require('express-handlebars')
+const path = require('path')
+
+
+// inicilizador
+const app = express()
+
+// settings
+app.set('port', process.env.PORT || 7000)
+app.set('views', path.join(__dirname, 'views'))
+app.engine('.hbs', exphbs({
+    defaultLayout: 'main',
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    partialsDir: path.join(app.get('views'), 'partials'),
+    extname: '.hbs',
+    helpers: require('./lib/handlebars')
+}))
+
+app.set('views engine', '.hbs')
+
+// middlewares
+app.use(morgan('dev'))
+app.use(express.urlencoded({extended: false}))
+app.use(express.json())
+
+// variables globales
+app.use((req, res, next) =>{
+    next()
+})
+
+// routes
+app.use(require('./routes/'))
+app.use(require('./routes/authentication'))
+app.use('/links',require('./routes/links'))
+
+
+// public
+app.use(express.static(path.join(__dirname, 'public')))
+
+// iniciar servidor
+app.listen(app.get('port'), () => {
+    console.log('Server on port', app.get('port'))
+})
---
+++ src/keys.js
@@ -0,0 +1,8 @@
+module.exports = {
+    database: {
+        host:  'localhost',
+        user:  'root',
+        password: 'Perfildev#19#_dev',
+        database: 'database_perfildev'
+    }
+}

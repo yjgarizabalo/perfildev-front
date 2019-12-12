b97c53cd1f475b98d64ac6042ce2553a06758e7f
yairg <yjgarizabalo@gmail.com>
2019-12-11 23:26:53 -0500

    login users in mysql

--- src/lib/passport.js
+++ src/lib/passport.js
@@ -4,6 +4,27 @@ const LocalStrategy = require('passport-local').Strategy
 const pool = require('../database')
 const helpers = require('../lib/helpers')
 
+passport.use('local.signin', new LocalStrategy({
+    usernameField: 'username',
+    passwordField: 'password',
+    passReqToCallback: true
+}, async (req, username, password, done) => {
+    console.log(req.body)
+    const rows = await pool.query('SELECT * FROM users WHERE username = ?', [username])
+    if(rows.length > 0) {
+        const user = rows[0]
+        helpers.matchPassword[0]
+        const validPasword = await helpers.matchPassword(password, user.password)
+        if (validPasword) {
+            done(null, user, req.flash('success', 'Bienvenidos' + user.username))
+        } else {
+            done(null, false, req.flash('message', 'Contraseña incorrecta'))
+        }
+    } else {
+        return done(null, false, req.flash('message','Este usuario no existe'))
+    }
+}))
+
 passport.use('local.signup', new LocalStrategy({
     usernameField: 'username',
     passwordField: 'password',
@@ -29,7 +50,7 @@ passport.serializeUser((user, done) => {
     done(null, user.id)
 })
 
-passport.deserializeUser( async (id, done) => {
-   const rows = await pool.query('SELECT * FROM users WHERE id = ?', [id])
-   done(null, rows[0])
+passport.deserializeUser(async (id, done) => {
+    const rows = await pool.query('SELECT * FROM users WHERE id = ?', [id])
+    done(null, rows[0])
 })
--- src/routes/authentication.js
+++ src/routes/authentication.js
@@ -4,17 +4,29 @@ const router = express.Router()
 const passport = require('passport')
 
 router.get('/signup', (req, res) => {
-   res.render('auth/signup.hbs')
+    res.render('auth/signup.hbs')
 })
 
-router.post('/signup', passport.authenticate('local.signup', { 
+router.post('/signup', passport.authenticate('local.signup', {
     successRedirect: '/profile',
     failureRedirect: '/signup',
     failureFlash: true
 }))
 
- router.get('/profile', (req, res) => {
-     res.send('Este es tu perfil')
- })
+router.get('/signin', (req, res) => {
+    res.render('auth/signin.hbs')
+})
+
+router.post('/signin', (req, res) => {
+    passport.authenticate('local.signin', {
+        successRedirect: '/profile',
+        failureRedirect: '/signin',
+        failureFlash: true
+    })(req, res);
+})
+
+router.get('/profile', (req, res) => {
+    res.send('Este es tu perfil')
+})
 
 module.exports = router

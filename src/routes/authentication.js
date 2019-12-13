d64e246b923f6256b1702acfd58d21c7dea3438e
yairg <yjgarizabalo@gmail.com>
2019-12-12 22:38:29 -0500

    proteger rutas server

--- src/routes/authentication.js
+++ src/routes/authentication.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 const passport = require('passport')
+const {isLoggedIn} = require('../lib/auth')
 
 router.get('/signup', (req, res) => {
     res.render('auth/signup.hbs')
@@ -25,8 +26,15 @@ router.post('/signin', (req, res) => {
     })(req, res);
 })
 
-router.get('/profile', (req, res) => {
-    res.send('Este es tu perfil')
+router.get('/profile', isLoggedIn, (req, res) => {
+    res.render('profile.hbs')
+})
+
+// Cerrar sesión
+
+router.get('/logout', (req, res) => {
+    req.logOut()
+    res.redirect('/signin')
 })
 
 module.exports = router
